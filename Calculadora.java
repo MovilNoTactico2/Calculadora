2b47fc13a8ad5eafde8ade72d3f91ee205250c18
MovilNoTactico2 <javiyt617@gmail.com>
2025-03-09 01:09:40 +0100

    Añadido metodo potencia

--- Calculadora.java
+++ Calculadora.java
@@ -31,6 +31,12 @@ public class Calculadora {
         return Math.pow(base, exponente);
     }
 
+    public static double media(int a, int b) {
+        System.out.println("El resultado es: ");
+        return (a + b) / 2.0;
+    }
+    
+
     public static double calculadora(int a, int b, int opcion) {
         switch (opcion) {
             case 1:
